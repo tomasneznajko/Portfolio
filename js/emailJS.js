ca6ff4941760545f7e2e9d7fdedf475722c5d72e
tomasneznajko <82177969+tomasneznajko@users.noreply.github.com>
2024-08-14 19:13:06 -0300

    Hora de la acción

---
+++ js/emailJS.js
@@ -0,0 +1,32 @@
+function sendEmail(){
+    console.log('Entra cuchillo')
+    var params = {
+        email: document.querySelector('#correo').value,
+        asunto: document.querySelector('#asunto').value,
+        mensaje: document.querySelector('#mensaje').value,
+    };
+    console.log('Salen las tripas');
+    const serviceID = 'service_57nu74b';
+
+    const templateID = 'template_egs2u8m';
+    console.log("Holaaa")
+    emailjs
+        .send(serviceID,templateID,params)
+        .then((res)=> {
+            document.querySelector('#correo').value = "";
+            document.querySelector('#asunto').value = "";
+            document.querySelector('#mensaje').value = "";
+            console.log(res);
+            alert("Su mensaje tititititirititi");
+            const modalElement = document.querySelector('#contacto');
+            const modal = bootstrap.Modal.getInstance(modalElement);
+            modal.hide();
+        })
+
+        .catch((err) => {
+            console.log(err);
+            alert("Oh no! Algo ha salido mal!");
+        })
+        
+}
+
