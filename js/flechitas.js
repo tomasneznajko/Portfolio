6a45040c40e1701a5230699750fa161a5fbd4624
tomasneznajko <82177969+tomasneznajko@users.noreply.github.com>
2025-01-08 22:25:54 -0300

    Added Taser via npm

--- js/flechitas.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.accordion-header').forEach(header => {
-    header.addEventListener('click', function() {
-        const item = this.parentElement;
-        item.classList.toggle('active');
-    });
-});
